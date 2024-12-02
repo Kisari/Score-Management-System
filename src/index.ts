3b99d2aaf2b971617e57f4a39baa1ec6544331a6
Kisari <seikakukisari@gmail.com>
2024-12-02 13:31:48 +0700

    docker setup

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ app.use(express.json());
 
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!!!!");
+  res.send("Hello World!!!");
 });
 
 app.listen(4000, () => {console.log("Server is running on port 4000")});
