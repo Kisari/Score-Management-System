4ac173eb69c85015ab61ed4fa09db5e4f965ce31
Kisari <seikakukisari@gmail.com>
2024-12-04 18:57:23 +0700

    fix next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,21 +1,7 @@
-import { NextConfig } from 'next';
-import path from 'path';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "app"),
-      "@/components": path.resolve(__dirname, "components"), 
-      "@/app": path.resolve(__dirname, "app"),
-    };
-    return config;
-  },
-
-  // Other Next.js configuration options
+  /* config options here */
 };
 
 export default nextConfig;
